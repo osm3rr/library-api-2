00216ac7abc8e52500b94d9aef108126ec16c646
osm3rr <osm3rr@adakademy.com>
2025-01-19 11:09:28 -0400

    books list done

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
